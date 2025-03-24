d3c115c3bc04b34dba095482b2df4afafa4e0078
HP ENVY <mayankdobriyal1920@gmail.com>
2025-03-24 18:16:18 +0530

    Push 92

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -64,7 +64,6 @@ const App = () => {
     const userSession = useSelector((state) => state.userSession);
     const {userInfo} = useSelector((state) => state.userAuthDetail);
 
-
     useEffect(() => {
         dispatch(actionToGetUserSessionData());
     }, []);
